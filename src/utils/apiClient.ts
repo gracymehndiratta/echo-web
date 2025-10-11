439415498c69af5a87012dfc276a7b5ccbf226d1
Labhesh <labheshk356@gmail.com>
2025-10-11 20:50:56 +0530

    Update apiClient.ts

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -5,7 +5,7 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL!;
 
 export const apiClient = axios.create({
     baseURL: API_BASE_URL,
-    timeout: 10000,
+    timeout: 100000,
     headers: {
       "Content-Type": "application/json",
     },
